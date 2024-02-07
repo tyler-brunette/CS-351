9d285f1312986fcaa1b7942f67140836f02ebd9b
Tyler Brunette <tybru02@gmail.com>
2024-02-07 10:22:49 -0500

    added beginning of page permissions

--- backend.py
+++ backend.py
@@ -48,16 +48,11 @@ def login():
             # Switch to redirect based on the user type
             if userType == "Manager":
                 response = make_response(redirect("/manager"))
-            elif userType == "Barista":
-                response = make_response(redirect("/barista"))
-            elif userType == "Cashier":
-                response = make_response(redirect("/cashier"))
+            elif userType == "Staff":
+                response = make_response(redirect("/scheduler"))
 
             # Set the username cookie for the user for logging in and out
-            response.set_cookie("Username", username)
-
-            # Automatically clock-in the user as they log in
-            DB.clockIn(username)
+            response.set_cookie("Role", userType)
 
             # Send the response to the client
             return response
@@ -71,168 +66,24 @@ def login():
 @app.route("/logout", methods=["GET"])
 def logout():
     # Get the username of the user
-    username = request.cookies.get("Username")
-
-    # Clock out the user
-    DB.clockOut(username)
+    response = None
+    response = make_response(redirect("/login"))
+    response.delete_cookie("Role")
 
     # Redirect to the login screen
-    return redirect("/login")
+    return response
 
 
 # Dashboard for the manager to access their different pages
 @app.route("/manager", methods=["GET"])
 def managerDashboard():
-    # Show the manager dashboard
-    return render_template("manager.html")
+    role = request.cookies.get("Role")
 
-
-# The default page for the cashier is the menu for placing an order
-@app.route("/cashier", methods=["GET", "POST"])
-def cashierDashboard():
-    # If the cashier is submitting an order, calculate the total price
-    if request.method == "POST":
-        # Tally the items
-        coffee_count = request.form.get("coffee")
-        deluxe_coffee_count = request.form.get("deluxe_coffee")
-        donut_count = request.form.get("donuts")
-
-        # Transaction number is simply (total number of transactions + 1)
-        transactionNum = DB.transactionDatabaseLength()
-
-        # The user will be redirected to the rewards step of the transaction after cookies are set
-        response = make_response(redirect("/rewards"))
-
-        # Cost of items for calculating total
-        costs = {"coffeeCost": 1.00, "deluxeCoffeeCost": 2.00, "donutCost": 2.50}
-
-        # The order object that will be placed into a cookie for better client-side processing
-        order = {
-            "orderNum": transactionNum,
-            "coffeeCount": coffee_count,
-            "deluxeCoffeeCount": deluxe_coffee_count,
-            "donutCount": donut_count,
-            "cost": (int(coffee_count) * costs.get("coffeeCost"))
-            + (int(deluxe_coffee_count) * costs.get("deluxeCoffeeCost"))
-            + (int(donut_count) * costs.get("donutCost")) * 1.06,
-            "status": "incomplete",
-        }
-
-        # Place the order object into a client cookie
-        response.set_cookie("Order", json.dumps(order))
-
-        # Send the response to the client
-        return response
+    if (role == "Manager"):
+        # Show the manager dashboard
+        return render_template("manager.html")
     else:
-        # If the user is visiting the cashier page
-
-        # After cookies are set, the cashier menu page will be displayed
-        response = make_response(render_template("cashier.html"))
-
-        # Clear cookies from previous orders if present
-        response.set_cookie("RewardDiscount", "", expires=0)
-        response.set_cookie("Order", "", expires=0)
-
-        # Send the response to the client
-        return response
-
-
-# The rewards step of the order submission process is asking for a phone number
-@app.route("/rewards", methods=["GET", "POST"])
-def rewardsDashboard():
-    # If the user is submitting a phone number for validation
-    if request.method == "POST":
-        # Get the phone number from the POST request
-        rewards_number = request.form.get("phone")
-
-        # When the phone number is processed, the client will be redirected to the payment screen
-        response = make_response(redirect("/payment"))
-
-        # Get the number of transactions the customer has completed, based on the phone number
-        customer_transactions = DB.checkCustomerCard(rewards_number)
-
-        # Increment the number of transactions completed
-        # Done in this step to limit the number of cookies stored by the user
-        DB.incrementCustomerCard(rewards_number)
-
-        # Set the cookie containing whether or not the customer has earned their rewards
-        response.set_cookie(
-            # Every fifth transaction is a free coffee (a dollar off total)
-            "RewardDiscount",
-            str(((customer_transactions + 1) % 5) == 0),
-        )
-
-        # Send the response to the client
-        return response
-
-    else:
-        # If the user is visiting the page, show the rewards page
-        return render_template("rewards.html")
-
-
-# The payment step of the order submission process
-# Allows cash or card. Both are simulated and care not stored
-@app.route("/payment", methods=["GET", "POST"])
-def paymentDashboard():
-    # If the user is submitting an order
-    if request.method == "POST":
-        # Get the order as a JSON object from the client
-        order = json.loads(request.cookies.get("Order"))
-
-        # Put the order into the database
-        DB.completeTransaction(order)
-
-        # Order is submitted, send the user back to cashier main page to start a new transaction
-        return redirect("/cashier")
-    else:
-        # Show the user the payment page
-        return render_template("payment.html")
-
-
-# Renders the inventory dashboard when a request is received
-@app.route("/inventory", methods=["GET", "POST"])
-def inventoryDashboard():
-    inventory = DB.printInventory()
-    if request.method == "POST":
-        print("This is a post request from Inventory")
-        return render_template("inventory.html")
-    else:
-        return render_template("inventory.html", inventory=inventory)
-
-
-# The default page for the barista with a list of incomplete order
-@app.route("/barista", methods=["GET"])
-def baristaDashboard():
-    # Get a list of incomplete orders to send to the client
-    incompleteOrders = DB.printBarista()
-
-    # There is no POST request for this endpoint (see next route for details)
-    # Display the barista main page and pass along data about all transactions
-    return render_template("barista.html", allTransactions=incompleteOrders)
-
-
-# The POST endpoint for marking an order as complete using it's order number
-@app.route("/barista/<orderNumber>", methods=["POST"])
-def update_order(orderNumber):
-    # Complete the order with the given order number
-    DB.completeOrder(orderNumber)
-
-    # Update the inventory to remove materials used in the given order
-    DB.updateInventory(orderNumber)
-
-    # Return a message to the client that the order was successfully updated
-    return jsonify({"message": f"Order {orderNumber} updated successfully"})
-
-
-# Page for viewing a history of past transactions
-@app.route("/transactionHistory", methods=["GET"])
-def transactionHistoryDashboard():
-    # Get all the transactions from the database
-    allTransactions = DB.printTransactions()
-
-    # There is no POST request for this endpoint
-    # Display the transaction history page and pass along data about all transactions
-    return render_template("transactionHistory.html", allTransactions=allTransactions)
+        return redirect("/logout")
 
 
 # Page for viewing all employee details
@@ -260,12 +111,6 @@ def schedulerDashboard():
     return render_template(
         "scheduler.html", allEmployees=allEmployees, schedules=schedules
     )
-
-
-# Reflection point for the cashier sending a new order to be received by the barista
-@socketIO.on("newOrder")
-def authConnection(order):
-    emit("newOrder", order, broadcast=True)
 
 
 # Start the Flask web server using sockets
--- database.py
+++ database.py
@@ -19,10 +19,7 @@ cluster = MongoClient(
 
 # Initializing the database in MongoDB
 db = cluster["schedule_system"]
-customer_collection = db["Customers"]
 employee_collection = db["Employees"]
-inventory_collection = db["Inventory"]
-transaction_collection = db["Transactions"]
 scheduler_collection = db["Scheduler"]
 
 
@@ -39,54 +36,8 @@ def createEmployee(name, password, title):
         "username": name,
         "pass": password,
         "title": title,
-        "clock_in": "",
-        "clock_out": "",
     }
     employee_collection.insert_one(post)
-
-
-# Runs when the employee logs in to work
-def clockIn(name):
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    employee_collection.update_one(
-        {"username": name}, {"$set": {"clock_in": str(current_time)}}
-    )
-    employee_collection.update_one({"username": name}, {"$set": {"clock_out": ""}})
-
-
-# Runs when the employee logs out
-def clockOut(name):
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    employee_collection.update_one(
-        {"username": name}, {"$set": {"clock_out": str(current_time)}}
-    )
-
-
-# Checks transactions on a customer card of a given phone number
-def checkCustomerCard(phoneNum):
-    for x in customer_collection.find({"phone": phoneNum}):
-        return x.get("transactions")
-    return 0
-
-
-# Makes a customer card if one was not found
-def createCustomerCard(phoneNum):
-    post = {"phone": phoneNum, "transactions": 0}
-    customer_collection.insert_one(post)
-
-
-# Increments the number
-def incrementCustomerCard(phoneNum):
-    numTransactions = 0
-    for x in customer_collection.find({"phone": phoneNum}):
-        numTransactions = x.get("transactions")
-        customer_collection.update_one(
-            {"transactions": numTransactions},
-            {"$set": {"transactions": numTransactions + 1}},
-        )
-    return 0
 
 
 # Prints all employees in the database
@@ -94,94 +45,6 @@ def printEmployees():
     return list(employee_collection.find())
 
 
-# Prints all transactions in the database
-def printTransactions():
-    return list(transaction_collection.find())
-
-
-# Gets the number of transactions in the database
-def transactionDatabaseLength():
-    return len(list(transaction_collection.find()))
-
-
-# Purpose: Prints all inventory items in the database
-def printInventory():
-    return list(inventory_collection.find())
-
-
-# Prints all incomplete orders in the transaction database
-def printBarista():
-    return list(transaction_collection.find({"status": "incomplete"}))
-
-
 # Prints all employee schedules in the scheduler database
 def printScheduler():
     return list(scheduler_collection.find())
-
-
-# Update an order from incomplete to complete
-def completeOrder(orderNum):
-    orderNumber = {"orderNum": int(orderNum)}
-    newStatus = {"$set": {"status": "complete"}}
-    transaction_collection.update_one(orderNumber, newStatus)
-
-
-# Add an order in the transaction database
-def completeTransaction(order):
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    post = {
-        "orderNum": order.get("orderNum"),
-        "regular": order.get("coffeeCount"),
-        "deluxe": order.get("deluxeCoffeeCount"),
-        "donut": order.get("donutCount"),
-        "cost": order.get("cost"),
-        "time": str(current_time),
-        "status": order.get("status"),
-    }
-    transaction_collection.insert_one(post)
-
-
-# Update the shop inventory
-def updateInventory(orderNum):
-    # Get the order from the customer
-    orderList = list(transaction_collection.find({"orderNum": int(orderNum)}))
-    order = orderList[0]
-
-    regOrder = int(order["regular"])
-    delOrder = int(order["deluxe"])
-    donutOrder = int(order["donut"])
-
-    # Calculating the amount of ingredients used on the order
-    cupsUsed = regOrder + delOrder
-    groundsUsed = (regOrder * 2) + (delOrder * 2)
-    milkUsed = delOrder * (0.05)
-
-    # Get the shop inventory database
-    inventoryList = list(inventory_collection.find())
-    inventory = inventoryList[0]
-
-    currentCups = inventory["cups"]
-    currentGrounds = inventory["coffee_grounds"]
-    currentMilk = inventory["milk"]
-    currentDonuts = inventory["donuts"]
-
-    # Calculate the new inventory amounts
-    totalCups = currentCups - cupsUsed
-    totalGrounds = currentGrounds - groundsUsed
-    totalMilk = currentMilk - milkUsed
-    totalDonuts = currentDonuts - donutOrder
-
-    # Update the inventory database
-    inventory_collection.update_one(
-        {"cups": currentCups}, {"$set": {"cups": totalCups}}
-    )
-    inventory_collection.update_one(
-        {"coffee_grounds": currentGrounds}, {"$set": {"coffee_grounds": totalGrounds}}
-    )
-    inventory_collection.update_one(
-        {"milk": currentMilk}, {"$set": {"milk": totalMilk}}
-    )
-    inventory_collection.update_one(
-        {"donuts": currentDonuts}, {"$set": {"donuts": totalDonuts}}
-    )
